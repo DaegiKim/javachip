5c373b842ebf98506599f8ca6cc928dd2133fbaa
DaegiKim <rmrhtdms@gmail.com>
2013-08-14 14:10:23 -0400

    Rename home controller

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -3,7 +3,7 @@ package controllers;
 import play.mvc.*;
 import views.html.home.index;
 
-public class Application extends Controller {
+public class Home extends Controller {
     public static Result index() {
         return ok(index.render());
     }
