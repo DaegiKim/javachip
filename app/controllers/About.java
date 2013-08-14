e3b990e61308313ae39c3a205c9eff9f07979ce7
DaegiKim <rmrhtdms@gmail.com>
2013-08-13 21:40:58 -0400

    remove comment by IDEA

--- app/controllers/About.java
+++ app/controllers/About.java
@@ -4,13 +4,6 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.about.index;
 
-/**
- * Created with IntelliJ IDEA.
- * User: daegikim
- * Date: 8/4/13
- * Time: 7:34 PM
- * To change this template use File | Settings | File Templates.
- */
 public class About extends Controller {
     public static Result index() {
         return ok(index.render());
